e393131f7e4155b85d2e8eba5d45cb90bf1753b1
unknown <manciefr@ENAC-IHM27.ihm>
2014-01-23 14:01:22 +0100

    Test de notifications

---
+++ src/testnotification/TestNotification.java
@@ -0,0 +1,121 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package testnotification;
+
+import java.awt.AWTException;
+import java.awt.CheckboxMenuItem;
+import java.awt.Image;
+import java.awt.MenuItem;
+import java.awt.PopupMenu;
+import java.awt.SystemTray;
+import java.awt.Toolkit;
+import java.awt.TrayIcon;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.awt.event.ItemEvent;
+import java.awt.event.ItemListener;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseListener;
+import javax.swing.JCheckBoxMenuItem;
+
+/**
+ *
+ * @author legranma
+ */
+public class TestNotification {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static CheckboxMenuItem barVisible;
+    private static ActionListener actionListener;
+    
+    public static void main(String[] args) {
+        final TrayIcon trayIcon;
+        
+        if (SystemTray.isSupported()) {
+
+            SystemTray tray = SystemTray.getSystemTray();
+            Image image = Toolkit.getDefaultToolkit().getImage("U:\\cadis.gif");
+
+            MouseListener mouseListener = new MouseListener() {
+                public void mouseClicked(MouseEvent e) {
+                    System.out.println("Tray Icon - Mouse clicked!");
+                }
+
+                public void mouseEntered(MouseEvent e) {
+                    System.out.println("Tray Icon - Mouse entered!");
+                }
+
+                public void mouseExited(MouseEvent e) {
+                    System.out.println("Tray Icon - Mouse exited!");
+                }
+
+                public void mousePressed(MouseEvent e) {
+                    System.out.println("Tray Icon - Mouse pressed!");
+                }
+
+                public void mouseReleased(MouseEvent e) {
+                    System.out.println("Tray Icon - Mouse released!");
+                }
+
+              
+            };
+
+            ActionListener exitListener = new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("Exiting...");
+                    System.exit(0);
+                }                          
+            };
+            
+            ItemListener changeStateBarListener = new ItemListener() {
+                public void actionPerformed(ActionEvent e) {
+                    System.out.println("fdtyh");
+                    System.out.println(barVisible.getState());
+                    
+                }
+
+                @Override
+                public void itemStateChanged(ItemEvent e) {
+                   System.out.println("fdtyh");
+                    System.out.println(barVisible.getState());
+                }
+            };
+
+            PopupMenu popup = new PopupMenu();
+            MenuItem defaultItem = new MenuItem("Exit");
+            defaultItem.addActionListener(exitListener);
+            popup.add(defaultItem);
+
+            barVisible = new CheckboxMenuItem("Afficher Barre");
+            barVisible.addItemListener((ItemListener) changeStateBarListener);
+            popup.add(barVisible);
+            
+            trayIcon = new TrayIcon(image, "Tray Demo", popup);
+
+            ActionListener actionItemListener = new ActionListener() {
+                public void actionPerformed(ActionEvent e) {
+                    trayIcon.displayMessage("Action Event",
+                            "An Action Event Has Been Performed!",
+                            TrayIcon.MessageType.INFO);
+                }
+            };
+
+            trayIcon.setImageAutoSize(true);
+            trayIcon.addActionListener(actionListener);
+            trayIcon.addMouseListener(mouseListener);
+
+            try {
+                tray.add(trayIcon);
+            } catch (AWTException e) {
+                System.err.println("TrayIcon could not be added.");
+            }
+
+        } else {
+            //  System Tray is not supported
+        }
+    }
+}
